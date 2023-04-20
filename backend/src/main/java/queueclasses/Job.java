66203b9601358545908cb30594655d8919169228
M-Kron <milokron@gmail.com>
2023-04-20 17:08:56 -0400

    .

--- backend/src/main/java/queueclasses/Job.java
+++ backend/src/main/java/queueclasses/Job.java
@@ -6,6 +6,6 @@ package queueclasses;
  * @param contact given by the user through the enqueue menu
  * @param printTime given by the user through the enqueue menu. Should be derived
  *                  from the Prusa Slicer estimate, but not checked
- * @param timeQueued recorded automaticaly
+ * @param timeQueued recorded automatically
  */
 public record Job(String user, String contact, double printTime, double timeQueued) {}
--- backend/src/main/java/queueclasses/QHandler.java
+++ backend/src/main/java/queueclasses/QHandler.java
@@ -1,5 +1,6 @@
 package queueclasses;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;

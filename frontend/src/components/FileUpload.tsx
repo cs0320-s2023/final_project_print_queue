3bf579e09d2b00203ed852573bd699fb5e06cb84
ipinedad <ivan_pineda-dominguez@brown.edu>
2023-05-06 16:49:59 -0400

    feat: Add timer synchronization using moment.js

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -142,7 +142,7 @@ function extractPrintTime(gcodeText: any) {
       const [hours, minutes, seconds] = timeString.split(" ");
       const totalSeconds =
         parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
-      printTime = `PT${totalSeconds}S`;
+      printTime = totalSeconds.toString();
       break;
     }
   }
